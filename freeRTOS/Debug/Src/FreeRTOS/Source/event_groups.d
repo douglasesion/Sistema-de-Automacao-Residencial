cb7af13d73ee685fa850513c7d102285bdc24e7a
Douglas de Souza Sesion <96123354+douglasesion@users.noreply.github.com>
2023-06-27 15:07:02 -0300

    Create event_groups.d

---
+++ freeRTOS/Debug/Src/FreeRTOS/Source/event_groups.d
@@ -0,0 +1,25 @@
+Src/FreeRTOS/Source/event_groups.o: ../Src/FreeRTOS/Source/event_groups.c \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/FreeRTOS.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOSConfig.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/portable.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/task.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/list.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/timers.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/task.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/event_groups.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/timers.h
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/FreeRTOS.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOSConfig.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/projdefs.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/portable.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/mpu_wrappers.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/task.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/list.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/timers.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/task.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/event_groups.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/timers.h:
