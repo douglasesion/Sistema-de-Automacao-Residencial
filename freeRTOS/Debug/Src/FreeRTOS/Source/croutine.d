d6c1673a3d1e443591d53644255ffcf58b80d7d1
Douglas de Souza Sesion <96123354+douglasesion@users.noreply.github.com>
2023-06-27 15:04:07 -0300

    Create croutine.d

---
+++ freeRTOS/Debug/Src/FreeRTOS/Source/croutine.d
@@ -0,0 +1,19 @@
+Src/FreeRTOS/Source/croutine.o: ../Src/FreeRTOS/Source/croutine.c \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/FreeRTOS.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOSConfig.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/portable.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/task.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/list.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/croutine.h
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/FreeRTOS.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOSConfig.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/projdefs.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/portable.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/mpu_wrappers.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/task.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/list.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/croutine.h:
