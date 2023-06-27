036a7d7473815c85ff69da947afbb853f820bab6
Douglas de Souza Sesion <96123354+douglasesion@users.noreply.github.com>
2023-06-27 15:09:52 -0300

    Create stm32f4_discovery.d

---
+++ freeRTOS/Debug/Src/stm32f4_discovery.d
@@ -0,0 +1,25 @@
+Src/stm32f4_discovery.o: ../Src/stm32f4_discovery.c \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/stm32f4_discovery.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_inc/stm32f4xx.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cm4.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cmInstr.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cmFunc.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cm4_simd.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_inc/system_stm32f4xx.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_StdPeriph_Driver_inc/stm32f4xx_gpio.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_StdPeriph_Driver_inc/stm32f4xx_rcc.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_StdPeriph_Driver_inc/stm32f4xx_exti.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_StdPeriph_Driver_inc/stm32f4xx_syscfg.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_StdPeriph_Driver_inc/misc.h
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/stm32f4_discovery.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_inc/stm32f4xx.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cm4.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cmInstr.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cmFunc.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cm4_simd.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_inc/system_stm32f4xx.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_StdPeriph_Driver_inc/stm32f4xx_gpio.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_StdPeriph_Driver_inc/stm32f4xx_rcc.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_StdPeriph_Driver_inc/stm32f4xx_exti.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_StdPeriph_Driver_inc/stm32f4xx_syscfg.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_StdPeriph_Driver_inc/misc.h:
