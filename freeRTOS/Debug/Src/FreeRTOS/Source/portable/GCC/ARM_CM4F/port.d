41118dfd647bd94724369c43494c24fd518127fd
Douglas de Souza Sesion <96123354+douglasesion@users.noreply.github.com>
2023-06-27 15:15:13 -0300

    Create port.d

---
+++ freeRTOS/Debug/Src/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.d
@@ -0,0 +1,18 @@
+Src/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.o: \
+ ../Src/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/FreeRTOS.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOSConfig.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/portable.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/task.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/list.h
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/FreeRTOS.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOSConfig.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/projdefs.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/portable.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/mpu_wrappers.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/task.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/list.h:
