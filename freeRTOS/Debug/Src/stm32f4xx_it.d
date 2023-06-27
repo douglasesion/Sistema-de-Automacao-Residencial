bf5236cf05dcdaa0bdd5d5713fea47a2d3fa1504
Douglas de Souza Sesion <96123354+douglasesion@users.noreply.github.com>
2023-06-27 15:09:09 -0300

    Create stm32f4xx_it.d

---
+++ freeRTOS/Debug/Src/stm32f4xx_it.d
@@ -0,0 +1,15 @@
+Src/stm32f4xx_it.o: ../Src/stm32f4xx_it.c \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/stm32f4xx_it.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_inc/stm32f4xx.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cm4.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cmInstr.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cmFunc.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cm4_simd.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_inc/system_stm32f4xx.h
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/stm32f4xx_it.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_inc/stm32f4xx.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cm4.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cmInstr.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cmFunc.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cm4_simd.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_inc/system_stm32f4xx.h:
