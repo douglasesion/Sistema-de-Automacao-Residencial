ac330cc5c1eb325b045d966f5255b0139159627d
Douglas de Souza Sesion <96123354+douglasesion@users.noreply.github.com>
2023-06-27 15:07:24 -0300

    Create timers.d

---
+++ freeRTOS/Debug/Src/FreeRTOS/Source/timers.d
@@ -0,0 +1,23 @@
+Src/FreeRTOS/Source/timers.o: ../Src/FreeRTOS/Source/timers.c \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/FreeRTOS.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOSConfig.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/portable.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/task.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/list.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/queue.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/timers.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/task.h
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/FreeRTOS.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOSConfig.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/projdefs.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/portable.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/mpu_wrappers.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/task.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/list.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/queue.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/timers.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/task.h:
