49e204686d2b5d9f3f65f45db800f18c24725634
Douglas de Souza Sesion <96123354+douglasesion@users.noreply.github.com>
2023-06-27 15:09:21 -0300

    Create misc.d

---
+++ freeRTOS/Debug/Src/STM32F4xx_StdPeriph_Driver/src/misc.d
@@ -0,0 +1,16 @@
+Src/STM32F4xx_StdPeriph_Driver/src/misc.o: \
+ ../Src/STM32F4xx_StdPeriph_Driver/src/misc.c \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_StdPeriph_Driver_inc/misc.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_inc/stm32f4xx.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cm4.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cmInstr.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cmFunc.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cm4_simd.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_inc/system_stm32f4xx.h
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_StdPeriph_Driver_inc/misc.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_inc/stm32f4xx.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cm4.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cmInstr.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cmFunc.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cm4_simd.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_inc/system_stm32f4xx.h:
