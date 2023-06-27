c14df8d707df20e0c5d1effe31dea549a991272f
Douglas de Souza Sesion <96123354+douglasesion@users.noreply.github.com>
2023-06-27 15:05:54 -0300

    Create queue.d

---
+++ freeRTOS/Debug/Src/FreeRTOS/Source/queue.d
@@ -0,0 +1,19 @@
+Src/FreeRTOS/Source/queue.o: ../Src/FreeRTOS/Source/queue.c \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/FreeRTOS.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOSConfig.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/portable.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/task.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/list.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/queue.h
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/FreeRTOS.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOSConfig.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/projdefs.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/portable.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/mpu_wrappers.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/task.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/list.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/FreeRTOS/Source/include/queue.h:
