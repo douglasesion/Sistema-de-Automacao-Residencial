b90318f217ae9b286499de3c647c00b8a50c143a
Douglas de Souza Sesion <96123354+douglasesion@users.noreply.github.com>
2023-06-27 15:20:35 -0300

    Create stm32f4xx_spi.d

---
+++ freeRTOS/Debug/Src/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_spi.d
@@ -0,0 +1,18 @@
+Src/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_spi.o: \
+ ../Src/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_spi.c \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_StdPeriph_Driver_inc/stm32f4xx_spi.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_inc/stm32f4xx.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cm4.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cmInstr.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cmFunc.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cm4_simd.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_inc/system_stm32f4xx.h \
+ C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_StdPeriph_Driver_inc/stm32f4xx_rcc.h
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_StdPeriph_Driver_inc/stm32f4xx_spi.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_inc/stm32f4xx.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cm4.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cmInstr.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cmFunc.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/CMSIS_inc/core_cm4_simd.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_inc/system_stm32f4xx.h:
+C:/Users/dsesi/STM32CubeIDE/freeRTOS/Inc/STM32F4xx_StdPeriph_Driver_inc/stm32f4xx_rcc.h:
